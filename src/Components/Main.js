ea430eeb4d384ab7d6625b1fb7ddc4da980da48e
sebas <sebas.drewes@gmail.com>
2021-08-15 12:56:12 -0300

    refactor to use custom hook

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,32 +1,16 @@
 import React, { useEffect } from 'react';
+//custom hook
+import { useHttp } from '../hooks/http'
 
 const Main = ({setScore, setGlobalScore}) => {
-
-    setScore(5);
-    setGlobalScore(10);
-    const randomNumber = Math.floor(Math.random() * (152 - 0)) + 0;
-    const fetchData = (number) => {
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-        .then(response => {
-            if (!response) {
-                throw new Error('Failed to fetch')
-            }
-            return response.json();
-        })
-        .then(pokeData => {
-            console.log(number)
-            console.log(pokeData.results[number].name);
-            console.log(pokeData.results[number+1].name);
-            console.log(pokeData.results[number+2].name);
-            console.log(pokeData.results[number+5].name);
-        })
-
-    }
-    
-
     useEffect(() => {
-        fetchData(randomNumber)
-    }, [randomNumber]);
+        setScore(5);
+        setGlobalScore(10);
+    }, [setScore, setGlobalScore]);
+    
+    const [isLoading, fetchedData] = useHttp('https://pokeapi.co/api/v2/pokemon?limit=151', []);
+    console.log(isLoading)
+    console.log(fetchedData.results[55])
 
     return (
         <div id="mainContainer"></div>
---
+++ src/hooks/http.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+export const useHttp = (url, dependencies) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [fetchedData, setFetchedData] = useState(null);
+    useEffect(() => {
+        setIsLoading(true);
+        (async () => {
+            try {
+            const data = await(await fetch(url, { mode: 'cors' })).json();
+            setFetchedData(data);
+            setIsLoading(false);
+            }
+            catch {
+                throw new Error('Failed to fetch')
+            }
+        
+            })();
+        }, [url], dependencies);
+    return [isLoading, fetchedData]
+};
