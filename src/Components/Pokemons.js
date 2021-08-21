08f4407744bed12168bc915ddd3fc7c5870a4e93
sebas <sebas.drewes@gmail.com>
2021-08-20 23:50:37 -0300

    terminada animacion card

--- src/Components/Pokemons.js
+++ src/Components/Pokemons.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState}from 'react';
 import './Pokemons.css'
 
 const Pokemons = ({pokemons, handleClick, setLoading}) => {
+    const [animationClass, setAnimationClass] = useState("cardImg")
     const isLoaded = (i) => {
         return pokemons[i].isLoaded = true
     }
@@ -10,25 +11,36 @@ const Pokemons = ({pokemons, handleClick, setLoading}) => {
             setLoading(false)
         }
     }
+    const capitalize = (string) => {
+    return string.replace(string[0], string[0].toUpperCase());
+    }
+    const animationClick = () => {
+        if(animationClass === "cardImg") {
+            setAnimationClass("cardImgClicked")
+        } else {
+            setAnimationClass("cardImg")
+        }
+        }
+
     const displayPokemons = () => {
         const pokemonsArray = []
         for (let i = 0; i < pokemons.length; i += 1) {
             const { id } = pokemons[i];
             pokemonsArray.push(
-            <div className="card" key={`card${id}`} onClick={() => handleClick(id)}>
+            <div className="card" key={`card${id}`} onClick={() => {handleClick(id); animationClick()}}>
                 <img 
-                className="cardImg"
+                className={animationClass}
                 key={`cardImg${id}`}
                 src={pokemons[i].img}
+                draggable="false"
                 onLoad={() => {isLoaded(i); checkIfAllLoaded()}}
                 alt={pokemons[i].name}>
                 </img>
                 <p
                 className="cardName"
                 key={`cardName${id}`}
-                value={pokemons[i].name}
                 >
-                {pokemons[i].name}
+                {capitalize(pokemons[i].name)}
                 </p>
             </div>
             );
